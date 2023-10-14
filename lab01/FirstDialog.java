ae704a193bb8adf72a292653ccf2ed416584d804
Anonymous <103519498+twilight2368@users.noreply.github.com>
2023-10-14 19:25:33 +0700

    now

---
+++ lab01/FirstDialog.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class FirstDialog {
+    public static void main(String arg[]){
+        JOptionPane.showMessageDialog(null, "Hello world! How are you?", null, 0);
+        System.exit(0);
+    }
+}
