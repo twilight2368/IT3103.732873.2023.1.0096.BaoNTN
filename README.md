6e9a22374ed532c5f75d20c1057964b21657f39a
Anonymous <103519498+twilight2368@users.noreply.github.com>
2023-11-10 13:23:17 +0700

    now

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # IT3103.732873.2023.1.0096.BaoNTN
-
-![Alt text](MicrosoftTeams-image.png)
--- lab01/HelloWorld.java
+++ lab01/HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
     public static void main(String arg[]){
         System.out.println("Xin chao \n cac ban");
         System.out.println("Hello \t world");
+
+        Math.random();
     }
 }
