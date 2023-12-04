d531051f4e3fbb4330a2225b61b87ed9dc7ffb2d
Anonymous <103519498+twilight2368@users.noreply.github.com>
2023-12-04 19:55:29 +0700

    nơ

--- lab03/AimsProject/AimsProject/DigitalVideoDisc.java
+++ lab03/AimsProject/AimsProject/DigitalVideoDisc.java
@@ -26,6 +26,27 @@ public class DigitalVideoDisc {
 	public float getCost() {
 		return cost;
 	}
+	
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	public void setCost(float cost) {
+		this.cost = cost;
+	}
 
 	public DigitalVideoDisc(String title) {
 		super();
