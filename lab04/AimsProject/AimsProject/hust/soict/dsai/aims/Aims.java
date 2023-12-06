b4c70eebb473b9af691a5167ff504d4fb12c45da
Anonymous <103519498+twilight2368@users.noreply.github.com>
2023-12-06 19:00:42 +0700

    now

---
+++ lab04/AimsProject/AimsProject/hust/soict/dsai/aims/Aims.java
@@ -0,0 +1,29 @@
+package hust.soict.dsai.aims;
+import hust.soict.dsai.aims.cart.Cart;
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+
+public class Aims {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cart anOrder = new Cart();
+		//anOrder.addDigitalVideoDisc(null);
+		
+		
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+		anOrder.addDigitalVideoDisc(dvd1);
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star War", "Sci-fi", "George Lucas", 87, 24.95f);
+		anOrder.addDigitalVideoDisc(dvd2);
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+		anOrder.addDigitalVideoDisc(dvd3);
+		
+		System.out.println("Total cost is: ");
+		System.out.println(anOrder.totalCost());
+		
+		DigitalVideoDisc dvd4 = new DigitalVideoDisc("Spider-man", "Action",  20f);
+		anOrder.addDigitalVideoDisc(dvd4);
+		anOrder.removeDigitalVideoDisc(dvd4);
+		
+	}
+
+}
