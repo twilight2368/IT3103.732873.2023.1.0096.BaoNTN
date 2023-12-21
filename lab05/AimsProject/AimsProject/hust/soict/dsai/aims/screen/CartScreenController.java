13fe309878d154bebe75fd306e7edad3b56aa03d
Anonymous <103519498+twilight2368@users.noreply.github.com>
2023-12-21 20:16:16 +0700

    now

--- lab05/AimsProject/AimsProject/hust/soict/dsai/aims/screen/CartScreenController.java
+++ lab05/AimsProject/AimsProject/hust/soict/dsai/aims/screen/CartScreenController.java
@@ -4,43 +4,74 @@ import javax.swing.JFrame;
 
 import hust.soict.dsai.aims.cart.Cart;
 import hust.soict.dsai.aims.media.Media;
+import hust.soict.dsai.aims.media.Playable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.Button;
 
 public class CartScreenController {
 	private Cart cart;
-	
+
 	@FXML
-    private TableView<Media> tblMedia;
-	
-    @FXML
-    private TableColumn<Media, String> colMediaCost;
+	private TableView<Media> tblMedia;
 
-    @FXML
-    private TableColumn<Media, String> colMediaTitle;
+	@FXML
+	private TableColumn<Media, String> colMediaCost;
 
-    @FXML
-    private TableColumn<Media, String> colMediaCategory;
+	@FXML
+	private TableColumn<Media, String> colMediaTitle;
 
-    @FXML
-    private ToggleGroup filterCategory;
+	@FXML
+	private TableColumn<Media, String> colMediaCategory;
+
+	@FXML
+	private ToggleGroup filterCategory;
+
+	@FXML
+	private Button btnPlay;
+
+	@FXML
+	private Button btnRemove;
 
 	public CartScreenController(Cart cart) {
 		// TODO Auto-generated constructor stub
 		super();
 		this.cart = cart;
 	}
-	
+
 	@FXML
 	private void initialize() {
 		colMediaTitle.setCellValueFactory(new PropertyValueFactory<Media, String>("title"));
 		colMediaCategory.setCellValueFactory(new PropertyValueFactory<Media, String>("category"));
 		colMediaCost.setCellValueFactory(new PropertyValueFactory<Media, String>("cost"));
-		
-		tblMedia.setItems(this.cart.getItemsOrdered());
-	}
 
+		tblMedia.setItems(this.cart.getItemsOrdered());
+
+		btnPlay.setVisible(false);
+		btnRemove.setVisible(false);
+
+		tblMedia.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Media>() {
+			@Override
+			public void changed(ObservableValue<? extends Media> observable, Media oldValue, Media newValue) {
+				if (newValue != null) {
+
+				}
+			}
+		});
+       
+	}
+	
+	void updateButtonBar(Media media) {
+		btnRemove.setVisible(true);
+		if (media instanceof Playable) {
+			btnPlay.setVisible(true);
+		}else {
+			btnPlay.setVisible(false);
+		}
+	}
 }
